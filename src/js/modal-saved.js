737ba780c83a83e71e4f6ec19027c3d431b11662
pavelkocharin <pavel.kocharin@gmail.com>
2019-06-19 01:40:41 +0500

    validation

--- src/js/modal-saved.js
+++ src/js/modal-saved.js
@@ -5,22 +5,14 @@
   var profile = document.querySelector('.main__profile');
 
   if (profile) {
-    var popup = document.querySelector('.popup-layout');
     var saveBtnClass = '.profile-item__form-button';
     var saveBtn = document.querySelector(saveBtnClass);
-    var savedField = document.querySelector('.popup-layout__saved');
     var saveCloseBtn = document.querySelector('.saved__close-btn');
-    var continueBtn = document.querySelector('.saved__button');
-    var popupActive = 'popup-layout--active';
-    var popupModalActive = 'popup-layout__modal-active';
-    var popupCloseBtn = document.querySelector('.popup-layout__popup-menu-close');
-    var popupCloseBtnUnactive = 'popup-layout__popup-menu-close--unactive';
     var formChangePass = document.querySelector('.profile-item__form-password');
     var sendFormBtn = formChangePass.querySelector('.profile-item__form-button');
     var errorText = formChangePass.querySelector('.profile-item__error-text');
     var errorTextActive = 'profile-item__error-text--active';
     var formInputs = formChangePass.querySelectorAll('.profile-item__form-input');
-
   
     function formIsValid () {
       Array.prototype.forEach.call(formInputs, function (elem) {
@@ -43,9 +35,7 @@
           event.preventDefault();
           return;
         } else {
-          popup.classList.add(popupActive);
-          savedField.classList.add(popupModalActive);
-          popupCloseBtn.classList.add(popupCloseBtnUnactive);
+          openModalSave();
         };
         
       });
@@ -53,29 +43,47 @@
       if (saveBtn) {
         saveBtn.addEventListener( 'click', function (event){
           event.preventDefault();
-          popup.classList.add(popupActive);
-          savedField.classList.add(popupModalActive);
-          popupCloseBtn.classList.add(popupCloseBtnUnactive);
+          openModalSave();
         });
       }
       
       if (saveCloseBtn) {
         saveCloseBtn.addEventListener( 'click', function (){
-          popup.classList.remove(popupActive);
-          savedField.classList.remove(popupModalActive);
+          closeModalSave();
         });
       };
       
       if (continueBtn) {
         continueBtn.addEventListener( 'click', function (){
-          popup.classList.remove(popupActive);
-          savedField.classList.remove(popupModalActive);
+          continiuBtnModal();
         });
       };
     }());
-
-
-    
   }
-  
 }());
+
+
+//глобальные переменные и функция для работы с модальным окном Данные сохранены (должно загружаться с сервера, в целях показа окна)
+var savedField = document.querySelector('.popup-layout__saved');
+var continueBtn = document.querySelector('.saved__button');
+var popupModalActive = 'popup-layout__modal-active';
+var popupCloseBtn = document.querySelector('.popup-layout__popup-menu-close');
+var popupCloseBtnUnactive = 'popup-layout__popup-menu-close--unactive';
+var popup = document.querySelector('.popup-layout');
+var popupActive = 'popup-layout--active';
+
+function openModalSave() {
+  popup.classList.add(popupActive);
+  savedField.classList.add(popupModalActive);
+  popupCloseBtn.classList.add(popupCloseBtnUnactive);
+};
+
+function closeModalSave() {
+  popup.classList.remove(popupActive);
+  savedField.classList.remove(popupModalActive);
+};
+
+function continiuBtnModal() {
+  popup.classList.remove(popupActive);
+  savedField.classList.remove(popupModalActive);
+};
--- src/js/registration.js
+++ src/js/registration.js
@@ -1,79 +1,113 @@
 'use strict';
-//функция модального окна "регистрация"
+//функция модальных окон
 
-(function () {
-  var popup = document.querySelector('.popup-layout');
-  var popupActive = 'popup-layout--active';
-  var closeMobileMenu = document.querySelector('.popup-layout__popup-menu-close');
-  var closeMobileMenuUnactive = 'popup-layout__popup-menu-close--unactive';
-  var menuMobileGroup = document.querySelector('.popup-layout__popup-menu');
-  var menu = document.querySelector('.header-nav__list');
-  var menuActive = 'header-nav__list--active';
-  var entryForm = document.querySelector('.header-form');
-  var entryFormActive = 'header-form--active';
-  var headerNav = document.querySelector('.header-nav');
-  var headerTop = document.querySelector('.header__top-content');
-  var openModalButtons = document.querySelectorAll('.open-modal');
-  var modalActiveClass = 'popup-layout__modal-active';
-  var closeModalButtons = document.querySelectorAll('.close-modal');
+var popup = document.querySelector('.popup-layout');
+var popupActive = 'popup-layout--active';
+var closeMobileMenu = document.querySelector('.popup-layout__popup-menu-close');
+var closeMobileMenuUnactive = 'popup-layout__popup-menu-close--unactive';
+var menuMobileGroup = document.querySelector('.popup-layout__popup-menu');
+var menu = document.querySelector('.header-nav__list');
+var menuActive = 'header-nav__list--active';
+var entryForm = document.querySelector('.header-form');
+var entryFormActive = 'header-form--active';
+var headerNav = document.querySelector('.header-nav');
+var headerTop = document.querySelector('.header__top-content');
+var openModalButtons = document.querySelectorAll('.open-modal');
+var modalActiveClass = 'popup-layout__modal-active';
+var closeModalButtons = document.querySelectorAll('.close-modal');
+var openedModal = document.querySelectorAll('.popup-layout__modal-active');
 
-  openModalButtons.forEach(function(button){
-    button.addEventListener('click', function(){
-      var id = button.getAttribute('data-id');
-      var modalWindow = document.getElementById(id);
-      openModal(id);
+Array.prototype.forEach.call(openModalButtons, (function(button){
+  button.addEventListener('click', function(){
+    var id = button.getAttribute('data-id');
+    openModal(id);
+  })
+}));
+
+function openModal (ident) {
+  var modalWindow = document.getElementById(ident);
+  var openedModal = document.querySelectorAll('.popup-layout__modal-active');
       
-      function openModal (id) {
+  if (!popup.classList.contains(popupActive)) {
+    popup.classList.add(popupActive);
+    closeMobileMenu.classList.add(closeMobileMenuUnactive);
+    modalWindow.classList.add(modalActiveClass);
 
-        var openedModale = document.querySelectorAll('.popup-layout__modal-active');
-        if (!popup.classList.contains(popupActive)) {
-          popup.classList.add(popupActive);
-          closeMobileMenu.classList.add(closeMobileMenuUnactive);
-          modalWindow.classList.add(modalActiveClass);
+  } else if (openedModal) {
+    Array.prototype.forEach.call(openedModal, (function(elem){
+      elem.classList.remove(modalActiveClass);
+    }));
 
-        } else if (openedModale) {
-          
-          openedModale.forEach(function(el){
-            el.classList.remove(modalActiveClass);
+    modalWindow.classList.add(modalActiveClass);
+    closeMobileMenu.classList.add(closeMobileMenuUnactive);
 
-          });
+    if (menuMobileGroup.firstElementChild) {
+      menuMobileGroup.removeChild(entryForm);
+      menuMobileGroup.removeChild(menu);
+    };
+  };
+};
 
-          modalWindow.classList.add(modalActiveClass);
-          closeMobileMenu.classList.add(closeMobileMenuUnactive);
+window.onresize = function updateOnresizeListener () {
+  if (popup.classList.contains(popupActive) && document.body.clientWidth > 830) {
+    menu.classList.remove(menuActive);
+    entryForm.classList.remove(entryFormActive);
+    popup.classList.add(popupActive);
+    headerNav.appendChild(menu);
+    headerTop.appendChild(entryForm);
+  };
+};
 
-          if (menuMobileGroup.firstElementChild) {
-            menuMobileGroup.removeChild(entryForm);
-            menuMobileGroup.removeChild(menu);
+Array.prototype.forEach.call(closeModalButtons, function(button){
+  button.addEventListener('click', function(){
+    closeModal();
+  });
+});
+
+function closeModal() {
+  var modalActiveClass = '.popup-layout__modal-active';
+  var modalWindow = document.querySelectorAll(modalActiveClass);
+
+  popup.classList.remove(popupActive);
+  Array.prototype.forEach.call(modalWindow, (function(elem){
+      elem.classList.remove('popup-layout__modal-active');
+  }));
+};                     
+
+
+//функция валидации полей форм (на предмет заполнения полей)
+(function () {
+var btnFormsValidate = document.querySelectorAll('.registration__button, .login__button, .recovery__button');
+var errorTextMess = document.querySelectorAll('.profile-item__error-text-validate');
+var errorTextMessActive = 'profile-item__error-text-validate--active';
+
+
+  Array.prototype.forEach.call(btnFormsValidate, (function(buttonValidate){
+    buttonValidate.addEventListener('click', function(event){
+      event.preventDefault();
+      var idVal = buttonValidate.getAttribute('data-id');
+      validateForm();
+      
+      function validateForm (idVal) {
+        var idVal = buttonValidate.getAttribute('data-id');
+        var formInputsValidate = document.getElementsByClassName("" + idVal + "");
+        
+        Array.prototype.forEach.call(formInputsValidate, function (elem) {
+          if (elem.value == 0) {
+            Array.prototype.forEach.call(errorTextMess, function (el) {
+              if (el.classList.contains("" + idVal + "")) {
+                el.classList.add(errorTextMessActive);
+              };
+            });
+            return false;
+          } else if (elem.value !== 0) {
+          return true;
           }
-        };
-
-        window.onresize = function() {
-          if (modalWindow.classList.contains(modalActiveClass) && document.body.clientWidth > 830) {
-            menu.classList.remove(menuActive);
-            entryForm.classList.remove(entryFormActive);
-            popup.classList.add(popupActive);
-            headerNav.appendChild(menu);
-            headerTop.appendChild(entryForm);
-          };
-        };
+        });
+      
       };
     })
-  });
+  }));
 
 
-  closeModalButtons.forEach(function(button){
-    button.addEventListener('click', function(){
-      closeModal();
-    });
-
-    function closeModal() {
-      var modalActiveClass = '.popup-layout__modal-active';
-      var modalWindow = document.querySelectorAll(modalActiveClass);
-
-      popup.classList.remove(popupActive);
-      modalWindow.forEach(function(elem){
-          elem.classList.remove('popup-layout__modal-active');
-      });
-    }
-  });
-}()); 
+}());
